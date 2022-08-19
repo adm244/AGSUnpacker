ece7b6ae3457c93c518da149970aae18b0911cd2
Adam Worgen <adm2444@gmail.com>
2022-08-20 00:57:41 +0300

    Version bump to 0.7

--- AGSUnpacker.UI/AppDescription.cs
+++ AGSUnpacker.UI/AppDescription.cs
@@ -3,7 +3,7 @@
   internal static class AppDescription
   {
     private static readonly int Major = 0;
-    private static readonly int Minor = 6;
+    private static readonly int Minor = 7;
 
     public static string ProgramName => "AGSUnpacker";
     public static string ProgramVersion => $"v{Major}.{Minor}";
