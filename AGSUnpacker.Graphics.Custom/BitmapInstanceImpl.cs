45b426103539298603944ee113f648e505d0a20e
Adam Worgen <adm2444@gmail.com>
2022-10-20 15:49:46 +0300

    Graphics: Stubbed custom image loading project

---
+++ AGSUnpacker.Graphics.Custom/BitmapInstanceImpl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using AGSUnpacker.Graphics.Formats;
+
+namespace AGSUnpacker.Graphics
+{
+  public class BitmapInstanceImpl : BitmapInstance
+  {
+    public BitmapInstanceImpl(string filepath)
+      : base(filepath)
+    {
+    }
+
+    public BitmapInstanceImpl(int width, int height, byte[] buffer, PixelFormat format, Palette? palette = null)
+      : base(width, height, buffer, format, palette)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override BitmapInstance Convert(PixelFormat format, bool discardAlpha)
+    {
+      throw new NotImplementedException();
+    }
+
+    public override byte[] GetPixels()
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void Save(string filepath, ImageFormat format)
+    {
+      throw new NotImplementedException();
+    }
+
+    protected override BitmapInstanceInfo LoadFromFile(string filepath)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
